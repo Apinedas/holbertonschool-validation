65f09f1af316803ddce83d92bfcb5a0e39b58c9f
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2023-01-09 10:56:27 -0500

    test 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y make wget gcc g++ git
-## wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
-## rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
-## export PATH=$PATH:/usr/local/go/bin
-## export PATH=$PATH:$HOME/go/bin
-## go install -tags extended github.com/gohugoio/hugo@latest
-## rm -f go1.19.4.linux-amd64.tar.gz
-## make build
+apt-get update && apt-get install -y make wget
+wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+dpkg -i hugo_extended_0.109.0_linux-amd64.deb
+rm -f hugo_extended_0.109.0_linux-amd64.deb
+make build
