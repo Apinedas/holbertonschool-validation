21f0442f9fc6e180837f72c9a129f4329e771c18
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2023-01-09 02:28:08 -0500

    Test setup

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y hugo make wget gcc g++ git
-apt-get remove -y hugo
-wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
+apt-get update && apt-get install -y make wget gcc g++ git >/dev/nul
+wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz >/dev/nul
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:$HOME/go/bin
-go install -tags extended github.com/gohugoio/hugo@latest
+go install -tags extended github.com/gohugoio/hugo@latest >/dev/nul
 rm -f go1.19.4.linux-amd64.tar.gz
-make build
+make build >/dev/nul
